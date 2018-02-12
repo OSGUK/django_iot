b8eeeb381c39894f019d77f8f34e65e8a3d99873
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2018-02-12 14:21:00 +0000

    fixing mac address parser

--- noisyatom/iot_device/urls.py
+++ noisyatom/iot_device/urls.py
@@ -11,5 +11,6 @@ from .views import(
 
 urlpatterns = [
     url(r'^register/$', iot_machines_register),
-    url(r'^machine/(?P<mac_address>[0-9]+)/$', iot_machine_find),
+    url(r'^machine/(?P<mac_address>[\w:\s]+)/$', iot_machine_find),
+    #url(r"^machine/(?P<mac>([0-9A-F]{2}[:-]){5}([0-9A-F]{2}))", iot_machine_find2),
 ]
